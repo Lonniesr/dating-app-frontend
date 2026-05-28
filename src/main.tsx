fe44c3e4b88849e661c399943c4baf95bce33707
Lonnie <iamadigga@gmail.com>
2026-05-28 02:34:12 -0400

    Remove React StrictMode duplicate socket mounts

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,34 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // ← ADD THIS LINE
+import "./index.css";
 
 import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
 import { UserAuthProvider } from "./user/context/UserAuthContext";
 import { ThemeProvider } from "./user/context/ThemeContext";
 
-// ✅ ADD THIS
 import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <UserAuthProvider>
-          <ThemeProvider>
-            <>
-              <App />
+ReactDOM.createRoot(
+  document.getElementById("root")!
+).render(
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <UserAuthProvider>
+        <ThemeProvider>
+          <>
+            <App />
 
-              {/* ✅ TOAST MOUNT POINT */}
-              <Toaster position="top-center" />
-            </>
-          </ThemeProvider>
-        </UserAuthProvider>
-      </QueryClientProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+            <Toaster position="top-center" />
+          </>
+        </ThemeProvider>
+      </UserAuthProvider>
+    </QueryClientProvider>
+  </BrowserRouter>
 );
