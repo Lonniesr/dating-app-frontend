0e838aa915acc088badd79ff4da6fda381411fb9
Lonnie <iamadigga@gmail.com>
2026-08-05 19:34:21 -0400

    Fix React chunk splitting

--- vite.config.ts
+++ vite.config.ts
@@ -79,13 +79,7 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           if (id.includes("node_modules")) {
-            if (
-              id.includes("react") ||
-              id.includes("react-dom") ||
-              id.includes("react-router")
-            ) {
-              return "react";
-            }
+            
 
             if (id.includes("@tanstack")) {
               return "query";
